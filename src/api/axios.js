103b429b2c92f5ae4783641e72770b6b317d9a1c
udaifa123 <udaifakk509@gmail.com>
2026-05-16 11:04:44 +0530

    first commit

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:4000/api';
+const API_URL = 'https://hostel-management-system-backened-1.onrender.com';
 
 const api = axios.create({
   baseURL: API_URL,
