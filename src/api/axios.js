a62d7696334c45237be0f361c0054af4359aacbe
udaifa123 <udaifakk509@gmail.com>
2026-05-16 18:02:45 +0530

    Fixed API_URL - hardcoded Render backend

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = 'https://hostel-management-system-backened-1.onrender.com/api';
 const api = axios.create({
   baseURL: API_URL,
   headers: {
